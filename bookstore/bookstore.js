71933fb773e319dab0c6c123329f507c3903fd41
Jeff Haddock <jeff.haddock.developer@gmail.com>
2024-10-22 17:47:42 -0400

    initial javascript file to support bookstore

---
+++ bookstore/bookstore.js
@@ -0,0 +1,31 @@
+let data = {
+    "books": [
+        { "title": "Tales of Horror", "author": "H.P. Lovecraft", "publishDate": "04/11/2017" },
+        { "title": "The Four Hour Body", "author": "Timothy Ferris", "publishDate": "09/15/1992" },
+        { "title": "The Count of Monte Cristo", "author": "Alexander Dumas", "publishDate": "01/15/1846" },
+        { "title": "Tales of Horror", "author": "H.P. Lovecraft", "publishDate": "04/11/2017" },
+        { "title": "The Four Hour Body", "author": "Timothy Ferris", "publishDate": "09/15/1992" },
+        { "title": "The Count of Monte Cristo", "author": "Alexander Dumas", "publishDate": "01/15/1846" },
+        { "title": "Tales of Horror", "author": "H.P. Lovecraft", "publishDate": "04/11/2017" },
+        { "title": "The Four Hour Body", "author": "Timothy Ferris", "publishDate": "09/15/1992" },
+        { "title": "The Count of Monte Cristo", "author": "Alexander Dumas", "publishDate": "01/15/1846" }
+    ],
+    "websiteTitle": "The Book Shoppe - a la JavaScript"
+};
+
+//Todo - use above data to output the javascript title:
+let mainPageTitle = document.querySelector('.main-title');
+
+let bookList = document.querySelector(".book-list");
+
+for(let i = 0; i < data.books.length; i++){
+   //TODO: work to build out "book cards" dynamically.
+    /* 
+        <div class="book-card">
+            <h3>The Book Title</h3>
+            Author: <span>James Kelley</span>
+            <p>Publish Date: 01/05/2025</p>
+        </div> 
+    */
+}
+
