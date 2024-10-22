091b90695bdc68f8d6c869ec6dffbc11308bb068
Jeff Haddock <jeff.haddock.developer@gmail.com>
2024-10-22 16:21:57 -0400

    final button click demo and discussion on DOM Manipulation

--- htmlFun/index.js
+++ htmlFun/index.js
@@ -1,1 +1,30 @@
 console.log("we made it to the javascript file!!! hooray! ")
+
+let counterPTag = document.querySelector("#counter");
+let clickMeButton = document.querySelector("button");
+let setCountToZero = document.querySelector("#zeroer");
+
+
+let incrementFunction = () => {
+    count++;
+    console.log("the count!!", count)
+    counterPTag.innerHTML = count;
+}
+
+let setToZero = () => {
+    count = 0;
+    counterPTag.innerHTML = count;
+}
+
+clickMeButton.addEventListener('click', incrementFunction)
+setCountToZero.addEventListener('click', setToZero)
+let count = 0;
+
+//OLDER method for creating functions:
+// function incrementOld () {
+
+// }
+
+//general guide to creating new code variables/functions
+//YAGNI
+// You Ain't Gonna Need It
--- sandbox.js
+++ sandbox.js
@@ -2,61 +2,61 @@
 
 // //Data types
 
-// //STRING
-// let someName = 'Trevor';
-// someName += " S";
-// console.log(someName);
+//STRING
+let someName = 'Trevor';
+someName += " S";
+console.log(someName);
 
-// //Numbers
-// let someNumber = 27;
-// someNumber = someNumber + 3;
-// let someRemainder = someNumber % 3;
-// let someDividend = someNumber / 3;
-// console.log(someDividend, someRemainder );
+//Numbers
+let someNumber = 27;
+someNumber = someNumber + 3;
+let someRemainder = someNumber % 3;
+let someDividend = someNumber / 3;
+console.log(someDividend, someRemainder );
 
-// //Non-primitive datatypes
-// //Objects 
-// let rose = {
-//     name: "rose",
-//     color: "red"
-// }
-// let tulip = {
-//     name: "tulip",
-//     color: "yellow", 
-//     // gallonsOfWater: 1,
-//     // someObject: {
-//     //     someKey: "someVariable"
-//     // }
-// }
-// console.log("The beautiful " + rose.name + " is " + rose.color);
-// console.log("The beautiful " + tulip.name + " is " + tulip.color);
+//Non-primitive datatypes
+//Objects 
+let rose = {
+    name: "rose",
+    color: "red"
+}
+let tulip = {
+    name: "tulip",
+    color: "yellow", 
+    // gallonsOfWater: 1,
+    // someObject: {
+    //     someKey: "someVariable"
+    // }
+}
+console.log("The beautiful " + rose.name + " is " + rose.color);
+console.log("The beautiful " + tulip.name + " is " + tulip.color);
 
-// //Array 
-// let listOfBookPages = [100, 270, 76];
-// let listOfBookTitles = ["Great Gatsby", "The Notebook", "IT"];
-// let listOfPlants = [rose, tulip];
+//Array 
+let listOfBookPages = [100, 270, 76];
+let listOfBookTitles = ["Great Gatsby", "The Notebook", "IT"];
+let listOfPlants = [rose, tulip];
 
-// console.log(listOfBookPages[0], listOfBookTitles[0], listOfPlants[0].name);
+console.log(listOfBookPages[0], listOfBookTitles[0], listOfPlants[0].name);
 
-// let pi = 3.14159;
-// pi += 8;
-// console.log(pi);
+let pi = 3.14159;
+pi += 8;
+console.log(pi);
 
-// console.log("our very first javascript message");
+console.log("our very first javascript message");
 
-// //Iterations
-// let secondListOfBookTitles = ["Great Gatsby", "The Notebook", "IT"];
+//Iterations
+let secondListOfBookTitles = ["Great Gatsby", "The Notebook", "IT"];
 
-// //push and pop
-// let myPoppedBook = secondListOfBookTitles.pop();
-// let anotherBook = "The Count of Monte Cristo";
-// secondListOfBookTitles.push(anotherBook);
+//push and pop
+let myPoppedBook = secondListOfBookTitles.pop();
+let anotherBook = "The Count of Monte Cristo";
+secondListOfBookTitles.push(anotherBook);
 
-// // for loop
-// for(let i = 0; i <= 2; i++) {
-//     let loggableString = secondListOfBookTitles[i];
-//     console.log(loggableString)
-// }
+// for loop
+for(let i = 0; i <= 2; i++) {
+    let loggableString = secondListOfBookTitles[i];
+    console.log(loggableString)
+}
 
 // THIS WILL NOT WORK!! IT IS OUT OF SCOPE!!
 //console.log(loggableString);
@@ -70,7 +70,20 @@ for(let i = 0; i < numbers.length; i++){
     console.log(remainder)
     if(remainder == 1){
         console.log("ODD!")
-    } else {
+    } else if(remainder == 2){
+        //condition will NOT occur
+    }
+    else {
         console.log("EVEN!")        
     }
 }
+
+
+let addNumbers = (number1, number2) => {
+    return number1 + number2;
+}
+
+let result1 = addNumbers(90, 34);
+let result2 = addNumbers(234, 324);
+let result3 = addNumbers(2, 5);
+console.log("Our numbers: ", result1, result2, result3)
