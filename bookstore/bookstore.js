76be7b0a0fa4b4682d4591605aa7b6e9a3f9c1f0
Jeff Haddock <jeff.haddock.developer@gmail.com>
2024-10-23 12:03:07 -0400

    adds book card elements to booklist dynamically :

--- bookstore/bookstore.js
+++ bookstore/bookstore.js
@@ -3,20 +3,21 @@ let data = {
         { "title": "Tales of Horror", "author": "H.P. Lovecraft", "publishDate": "04/11/2017" },
         { "title": "The Four Hour Body", "author": "Timothy Ferris", "publishDate": "09/15/1992" },
         { "title": "The Count of Monte Cristo", "author": "Alexander Dumas", "publishDate": "01/15/1846" },
-        { "title": "Tales of Horror", "author": "H.P. Lovecraft", "publishDate": "04/11/2017" },
         { "title": "The Four Hour Body", "author": "Timothy Ferris", "publishDate": "09/15/1992" },
         { "title": "The Count of Monte Cristo", "author": "Alexander Dumas", "publishDate": "01/15/1846" },
-        { "title": "Tales of Horror", "author": "H.P. Lovecraft", "publishDate": "04/11/2017" },
-        { "title": "The Four Hour Body", "author": "Timothy Ferris", "publishDate": "09/15/1992" },
-        { "title": "The Count of Monte Cristo", "author": "Alexander Dumas", "publishDate": "01/15/1846" }
+        { "title": "The Iliad", "author": "Homer", "publishDate": "-800 BC"},
+        { "title": "Little Women", "author": "Louisa May Alcott", "publishDate": "05/17/1907"}
     ],
     "websiteTitle": "The Book Shoppe - a la JavaScript"
 };
 
-//Todo - use above data to output the javascript title:
+
 let mainPageTitle = document.querySelector('.main-title');
+mainPageTitle.innerHTML = data.websiteTitle
 
 let bookList = document.querySelector(".book-list");
+// console.log("The ole' booklist", bookList)
+
 
 for(let i = 0; i < data.books.length; i++){
    //TODO: work to build out "book cards" dynamically.
@@ -27,5 +28,29 @@ for(let i = 0; i < data.books.length; i++){
             <p>Publish Date: 01/05/2025</p>
         </div> 
     */
+    let bookData = data.books[i];
+
+    let bookDiv = document.createElement("div");
+    bookDiv.classList.add("book-card")
+    // <div class="book-card"> <div>
+
+    let bookHeader = document.createElement("h3");
+    bookHeader.innerHTML = bookData.title;
+    // <h3>Author Name</h3>
+  
+
+    let authorPTag = document.createElement('p');
+    authorPTag.innerHTML = "Author: " + data.books[i].author;
+    
+    let publishDatePTag = document.createElement('p');
+    publishDatePTag.innerHTML = "Publish Date: " + data.books[i].publishDate;
+        
+    bookDiv.appendChild(bookHeader);
+    bookDiv.appendChild(authorPTag);
+    bookDiv.appendChild(publishDatePTag);
+
+    //Adds book to the bookList
+    bookList.appendChild(bookDiv);
+
 }
 
